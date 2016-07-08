4a7a709fd358f43b646d2cd73994636c438132a6
SekiSelu <sekisunfire1001@hotmail.com>
2016-07-08 12:11:08 -0600

    Storage priority decrease

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -15,17 +15,18 @@ var roleHarvester = {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER ||
-								structure.structureType == STRUCTURE_STORAGE) &&
-									(structure.energy != undefined && (structure.energy < structure.energyCapacity) ||
-									 structure.store != undefined && (structure.store[RESOURCE_ENERGY] < structure.storeCapacity));
+                                structure.structureType == STRUCTURE_TOWER) && (structure.energy < structure.energyCapacity);
                     }
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
-            } else {
+            } else if (creep.room.storage) {
+				if(creep.transfer(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(creep.room.storage);
+				}
+			} else {
                 creep.moveTo(Game.flags['HarvesterRally']);
             }
         } else {
